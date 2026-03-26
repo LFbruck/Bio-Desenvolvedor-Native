fd82a2b2d669f650c3d5175bf56aeaca313bfe4d
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-03-25 22:17:45 -0300

    Criação da Home e Tela de Perfil

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,1 +1,34 @@
-export default function HomeScreen() {}
+import React from 'react'
+import {View,Text,Button, StyleSheet} from 'react-native';
+
+export default function HomeScreen({navigation}) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}> Luiz Filipe Bruck Reis Mattos - Desenvolvedor Full Stack</Text>
+            <Text style={styles.summary}>Engenheiro de Software em formação pelo Instituo Infnet, com foco em desenvolvimento Full Stack (React, TypeScript e Python) e construção de soluções escaláveis com AI Agents. </Text>
+            <Button  title={"Ver perfil Completo"}   onPress={() => navigation.navigate("Profile")}></Button>
+
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 20,
+        },
+        title: {
+            fontsize: 22,
+            fontWeight: 'bold',
+            marginBottom: 10,
+        },
+        summary: {
+            textAlign: 'center',
+            marignBottom: 20,
+            fontSize: 16,
+        }
+    })
+
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,1 +1,25 @@
-export default function ProfileScreen() {}
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+
+
+export default function ProfileScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Helloo world</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    title: {
+        fontsize: 22,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+})
