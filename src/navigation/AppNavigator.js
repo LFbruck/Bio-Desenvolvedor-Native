16553c8e10095f2fc1649686fed5b3338e6802d7
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-03-25 21:59:15 -0300

    Implementação de Navegação Stack

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+    return(
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} options={{title:'Inicio'}} />
+            <Stack.Screen name="Profile" component={ProfileScreen} options={{title: "Meu Perfil"}} />
+        </Stack.Navigator>
+
+    </NavigationContainer>)
+}
+
